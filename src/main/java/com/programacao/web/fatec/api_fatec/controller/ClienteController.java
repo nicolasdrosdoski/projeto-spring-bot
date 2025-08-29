423b7c01e194051fb32478f94b596f7a319e2313
NicolasGDS <jarbascow@gmail.com>
2025-08-28 21:58:58 -0300

    Tarefa CRUD

--- src/main/java/com/programacao/web/fatec/api_fatec/controller/ClienteController.java
+++ src/main/java/com/programacao/web/fatec/api_fatec/controller/ClienteController.java
@@ -14,6 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.programacao.web.fatec.api_fatec.domain.cliente.ClienteRepository;
 import com.programacao.web.fatec.api_fatec.entities.Cliente;
+
+import jakarta.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.PutMapping;
 
 
@@ -27,56 +30,96 @@ public class ClienteController {
     @Autowired
     private ClienteRepository clienteRepository;
 
-    public ClienteController() {
-        listaDeCliente.add(new Cliente(94L, "Nicolas", "Rua João Barth"));
-        
+    // public ClienteController() {
+    //     listaDeCliente.add(new Cliente(94L, "Nicolas", "Rua João Barth"));
+    // }
+
+    @PostConstruct
+    public void dadosIniciais(){
+        clienteRepository.save(new Cliente(null, "Nicolas", "Rua Barth 1"));
+        clienteRepository.save(new Cliente(null, "Maria", "Rua Barth 2"));
+        clienteRepository.save(new Cliente(null, "Eduarda", "Rua Barth 3"));
+        clienteRepository.save(new Cliente(null, "Joao", "Rua Barth 4"));
     }
 
-    @GetMapping("/cpf/{idade}")
-    public String clienteCpf(@PathVariable int idade){
-        if(idade >= 18){
-            return "Cliente maior de idade";
-        } else if(idade < 0) {
-            return "Ta morto!";
-        } else{
-            return "Cliente menor de idade";
+    
+
+    //* CRUD - Leitura de clientes
+    @GetMapping("/listar")
+    public List<Cliente> listarClientes(){
+        return clienteRepository.findAll();
+    }
+
+    //* CRUD - Leitura de clientes por nome
+    @GetMapping("/{nome}")
+    public List<Cliente> listarClientesPorNome(@PathVariable String nome){
+        return clienteRepository.findByNome(nome);
+    }
+
+    //* CRUD - Criar Cliente
+    @PostMapping("/criarCliente")
+    public String criarCliente(@RequestBody Cliente cliente){
+        try {
+            clienteRepository.save(cliente);
+        return "Cliente salvo";
+        } catch (Exception e) {
+            System.out.printf("Erro: %s", e);
+            return "Erro ao salvar cliente";
         }
     }
 
-    @GetMapping("/listar")
-    public List<Cliente> listarClientes() {
-        return listaDeCliente;
-    }
-    
-
-    @GetMapping("/cnpj/{nome}")
-    public String clienteCnpj(@PathVariable String nome){
-        return "Cliente CNPJ: " + nome;
-    }
-
-    @PostMapping("")
-    public Cliente createCliente(@RequestBody Cliente cliente){
-
-        return cliente;
-    }
-
-    @PostMapping("/criarCliente")
-    public List<Cliente> criarCliente(@RequestBody Cliente cliente) {
-        listaDeCliente.add(new Cliente(cliente.getId(), cliente.getNome(), cliente.getEndereco()));
-        return listaDeCliente;
+    //* CRUD - Atualizar cliente
+    @PutMapping("/atualizar")
+    public String atualizarCliente(@RequestBody Cliente cliente){
+        try {
+            clienteRepository.save(cliente);
+            return "Cliente atualizado com sucesso";
+        } catch (Exception e) {
+            System.out.printf("Erro: %s", e);
+            return "Erro ao atualizar cliente";
+        }
     }
 
     @DeleteMapping("/deletar/{id}")
     public String deletarCliente(@PathVariable Long id){
-        for (Cliente cliente : listaDeCliente) {
-            if(cliente.getId() == id){
-                listaDeCliente.remove(cliente);
-                return id + ": deletado!";
-            }
+        try {
+            clienteRepository.deleteById(id);
+            return "Cliente deletado com sucesso";
+        } catch (Exception e) {
+            System.out.printf("Erro: %s", e);
+            return "Erro ao deletar cliente";
         }
-        return "ID " + id + " não encontrado";
     }
-    
+
+    // @GetMapping("/cpf/{idade}")
+    // public String clienteCpf(@PathVariable int idade){
+    //     if(idade >= 18){
+    //         return "Cliente maior de idade";
+    //     } else if(idade < 0) {
+    //         return "Ta morto!";
+    //     } else{
+    //         return "Cliente menor de idade";
+    //     }
+    // }
+
+
+    // @GetMapping("/cnpj/{nome}")
+    // public String clienteCnpj(@PathVariable String nome){
+    //     return "Cliente CNPJ: " + nome;
+    // }
+
+    // @PostMapping("")
+    // public Cliente createCliente(@RequestBody Cliente cliente){
+
+    //     return cliente;
+    // }
+
+    // @PostMapping("/criarCliente")
+    // public List<Cliente> criarCliente(@RequestBody Cliente cliente) {
+    //     listaDeCliente.add(new Cliente(cliente.getId(), cliente.getNome(), cliente.getEndereco()));
+    //     return listaDeCliente;
+    // }
+
     @PutMapping("/atualizar/{id}")
     public String alterarCliente(@PathVariable Long id, @RequestBody Cliente entity) {
         for (Cliente cliente : listaDeCliente) {
@@ -88,6 +131,21 @@ public class ClienteController {
         }
         return "Cliente não encontrado";
     }
+
+    // @DeleteMapping("/deletar/{id}")
+    // public String deletarCliente(@PathVariable Long id){
+    //     for (Cliente cliente : listaDeCliente) {
+    //         if(cliente.getId() == id){
+    //             listaDeCliente.remove(cliente);
+    //             return id + ": deletado!";
+
+    //         }
+    //     }
+    //     return "ID " + id + " não encontrado";
+    // }
+
+    
+    
 
     
 }
--- src/main/java/com/programacao/web/fatec/api_fatec/domain/cliente/ClienteRepository.java
+++ src/main/java/com/programacao/web/fatec/api_fatec/domain/cliente/ClienteRepository.java
@@ -1,9 +1,12 @@
 package com.programacao.web.fatec.api_fatec.domain.cliente;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.programacao.web.fatec.api_fatec.entities.Cliente;
 
+
 public interface ClienteRepository extends JpaRepository<Cliente, Long>{
-    
+    List<Cliente> findByNome(String nome);
 }
